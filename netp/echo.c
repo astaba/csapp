77837a2ffccf56f62b0aaa496d9dcaea62556f34
Astar <bahouidi99@gmail.com>
2026-04-06 23:59:07 +0100

    Ch11: Update notes

--- netp/echo.c
+++ netp/echo.c
@@ -1,6 +1,6 @@
 /* =========================================================================
  * Created on: <Mon Mar 30 22:38:37 +01 2026>
- * Time-stamp: <Mon Mar 30 22:43:55 +01 2026 by owner>
+ * Time-stamp: <Mon Apr  6 21:04:16 +01 2026 by owner>
  * Author    : CS:APP
  * Desc      : ~/coding/c_prog/csapp/netp/echo.c -
  * Figure 11.22 read and echo text lines until client closes connection.
@@ -14,7 +14,7 @@ void echo(int connfd) {
   rio_t rio;
 
   Rio_readinitb(&rio, connfd);
-  while ((n = Rio_readlineb(&rio, buf, MAXLINE)) != 0) { // line:netp:echo:eof
+  while ((n = Rio_readlineb(&rio, buf, MAXLINE)) != 0) {
     printf("server received %d bytes\n", (int)n);
     Rio_writen(connfd, buf, n);
   }
